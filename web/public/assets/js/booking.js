ebdd000aa1c9944cdd596a0fd85a7fd8fcaadefd
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-01-11 22:37:35 +0000

    calander clickable and comes up with a box prepared to be a booking box

--- web/public/assets/js/booking.js
+++ web/public/assets/js/booking.js
@@ -1,1 +1,57 @@
-$(document).ready(function(){});
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+    let datePicker = document.getElementById("date_picker");
+
+    let timeBox = document.getElementsByClassName("timeBox");
+    let bookableSlot = document.getElementById("booking slot");
+
+    
+
+
+//on load, the page sets the date to today and makes days before today un-selectable.
+    let today = new Date();
+
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    datePicker.value = today;
+    datePicker.min = today;
+
+//gets time chosen from clickable time and opens bookables
+    let getTime = function(timeBox) {
+        return function() {
+            let chosenTime = timeBox.id;
+            let chosenSlot = document.getElementById(chosenTime);
+            chosenSlot.classList.add("active");
+
+            splitDate = datePicker.value.split("-");
+            theDate = splitDate[2] + "/" + splitDate[1] + "/" + splitDate[0];
+
+            bookableSlot.innerHTML = "";
+
+            let header = document.createElement("div");
+            header.classList.add("headerTimeCard");
+            header.innerText = theDate + " " + chosenTime;
+
+            let bookable = document.createElement("div");
+            bookable.classList.add("bookingTimeCard");
+
+            bookableSlot.append(header);
+            bookableSlot.append(bookable);
+
+            setTimeout (function() {
+                chosenSlot.classList.remove("active");
+            }, 1500);
+        }
+    };
+
+    for (var i = 0, l = timeBox.length; l > i; i++) {
+        timeBox[i].onclick = getTime(timeBox[i]);
+    }
+
+
+    
+});
--- web/public/booking.php
+++ web/public/booking.php
@@ -5,48 +5,50 @@
     <link rel="stylesheet" href="./assets/css/booking/style.css">
     <link rel="stylesheet" href="./assets/css/global.css">
     
-    
+    <script src="./assets/js/booking.js"></script>
 </head>
 
 <body>
     <div class="bookingHeader">
-        <button class="datePicker" placeholder="Select Date" readonly>Select Date
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"/></svg></button>
+        <input id="date_picker" class="datePicker" type="date" name="date_picker" required>
     </div>
+    
     <div class="bookingCard">
-            <div class="timeBox">08:30</div>
-            <div class="timeBox">08:45</div>
-            <div class="timeBox">09:00</div>
-            <div class="timeBox">10:00</div>
-            <div class="timeBox">10:15</div>
-            <div class="timeBox">10:30</div>
-            <div class="timeBox">10:45</div>
-            <div class="timeBox">11:00</div>
-            <div class="timeBox">11:15</div>
-            <div class="timeBox">11:30</div>
-            <div class="timeBox">11:45</div>
-            <div class="timeBox">12:00</div>
-            <div class="timeBox">12:15</div>
-            <div class="timeBox">12:30</div>
-            <div class="timeBox">12:45</div>
-            <div class="timeBox">13:00</div>
-            <div class="timeBox">13:15</div>
-            <div class="timeBox">14:30</div>
-            <div class="timeBox">14:45</div>
-            <div class="timeBox">15:00</div>
-            <div class="timeBox">15:15</div>
-            <div class="timeBox">15:30</div>
-            <div class="timeBox">15:45</div>
-            <div class="timeBox">16:00</div>
-            <div class="timeBox">16:15</div>
-            <div class="timeBox">16:30</div>
-            <div class="timeBox">16:45</div>
-            <div class="timeBox">17:00</div>
-            <div class="timeBox">17:15</div>
-            <div class="timeBox">17:30</div>
-            <div class="timeBox">17:45</div>
-            <div class="timeBox">18:00</div>
-            <div class="timeBox">18:15</div>   
+            <div class="timeBox" id="08:30">08:30</div>
+            <div class="timeBox" id="08:45">08:45</div>
+            <div class="timeBox" id="09:00">09:00</div>
+            <div class="timeBox" id="10:00">10:00</div>
+            <div class="timeBox" id="10:15">10:15</div>
+            <div class="timeBox" id="10:30">10:30</div>
+            <div class="timeBox" id="10:45">10:45</div>
+            <div class="timeBox" id="11:00">11:00</div>
+            <div class="timeBox" id="11:15">11:15</div>
+            <div class="timeBox" id="11:30">11:30</div>
+            <div class="timeBox" id="11:45">11:45</div>
+            <div class="timeBox" id="12:00">12:00</div>
+            <div class="timeBox" id="12:15">12:15</div>
+            <div class="timeBox" id="12:30">12:30</div>
+            <div class="timeBox" id="12:45">12:45</div>
+            <div class="timeBox" id="13:00">13:00</div>
+            <div class="timeBox" id="13:15">13:15</div>
+            <div class="timeBox" id="14:30">14:30</div>
+            <div class="timeBox" id="14:45">14:45</div>
+            <div class="timeBox" id="15:00">15:00</div>
+            <div class="timeBox" id="15:15">15:15</div>
+            <div class="timeBox" id="15:30">15:30</div>
+            <div class="timeBox" id="15:45">15:45</div>
+            <div class="timeBox" id="16:00">16:00</div>
+            <div class="timeBox" id="16:15">16:15</div>
+            <div class="timeBox" id="16:30">16:30</div>
+            <div class="timeBox" id="16:45">16:45</div>
+            <div class="timeBox" id="17:00">17:00</div>
+            <div class="timeBox" id="17:15">17:15</div>
+            <div class="timeBox" id="17:30">17:30</div>
+            <div class="timeBox" id="17:45">17:45</div>
+            <div class="timeBox" id="18:00">18:00</div>
+            <div class="timeBox" id="18:15">18:15</div>   
     </div> 
+
+    <div id="booking slot"></div>
 </body>
 </html>
