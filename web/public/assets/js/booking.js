39419201731659566497a95fe44951863ab3e0e2
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-02-15 11:01:20 +0000

    read functionality
    now used, boxes are auto blocked out when booked or blocked out. data is auto-filled

--- web/public/assets/js/booking.js
+++ web/public/assets/js/booking.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
     let datePicker = document.getElementById("date_picker");
 
     let timeBox = document.getElementsByClassName("timeBox");
@@ -16,6 +15,73 @@ document.addEventListener("DOMContentLoaded", () => {
     today = yyyy + '-' + mm + '-' + dd;
     datePicker.value = today;
     datePicker.min = today;
+
+    let bookedSlots = {};
+    let blockedSlots = {};
+
+    let checkAvailability = function() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                try {
+                    let json = xhr.responseText.substring(7);
+                    let bookings = JSON.parse(json).data;
+                    let keys = Object.keys(bookings);
+
+                    keys.map(key => {
+                        if (bookings[key].blocked == 0) {
+                            bookedSlots[bookings[key].bookingDate] = bookings[key];
+                        } else {
+                            blockedSlots[bookings[key].bookingDate] = bookings[key];
+                        }
+                    });
+
+                    let keysOfBooked = Object.keys(bookedSlots);
+                    keysOfBooked.map(key => {
+                        let bookingDate = bookedSlots[key].bookingDate;
+                        let patientID = bookedSlots[key].patientID;
+                        let reason = bookedSlots[key].reason;
+
+                        bookingDate = bookingDate.split(' ');
+                        let bookingBlockDate = bookingDate[0];
+                        let bookingBlockTime = bookingDate[1];
+                        if (datePicker.value == bookingBlockDate) {
+                            bookingBlockTime = bookingBlockTime.substring(0, bookingBlockTime.length - 3);
+                            let  slot = document.getElementById(bookingBlockTime);
+                            slot.classList.remove("timeBox");
+                            slot.classList.add("unselectableTime");
+                        }
+                    });
+
+                    let keysOfBlocked = Object.keys(blockedSlots);
+                    keysOfBlocked.map(key => {
+                        let bookingDate = blockedSlots[key].bookingDate;
+
+                        bookingDate = bookingDate.split(' ');
+                        let bookingBlockDate = bookingDate[0];
+                        let bookingBlockTime = bookingDate[1];
+                        if (datePicker.value == bookingBlockDate) {
+                            bookingBlockTime = bookingBlockTime.substring(0, bookingBlockTime.length - 3);
+                            let  slot = document.getElementById(bookingBlockTime);
+                            slot.classList.remove("timeBox");
+                            slot.classList.add("unselectableTime");
+                            slot.name = "blocked";
+                        }
+                    });
+
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        });
+
+        xhr.open("GET", "./api/booking/readAll.php?", true);
+        xhr.send();
+    }
+
+    checkAvailability();
+    console.log(bookedSlots);
 
     //gets time chosen from clickable time and opens bookables
     let getTime = function(timeBox) {
@@ -52,48 +118,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     let fillBookableForm = function(bookable, slot) {
-        let patientInputContainer = document.createElement("div");
-        let patientIDLabelClassifier = document.createElement("div");
-        let patientIDLabel = document.createElement("span");
-        let patientIDInput = document.createElement("input");
-        patientInputContainer.classList.add("questionContainer");
-        patientIDLabelClassifier.classList.add("questionLabel");
-        patientIDInput.classList.add("inputBox");
-        patientIDInput.id = "PatientIDInput";
-        patientIDInput.placeholder = "Patient ID";
-        patientIDLabel.innerHTML = "Patient ID:";
-        patientIDLabelClassifier.append(patientIDLabel);
-        patientInputContainer.append(patientIDLabelClassifier);
-        patientInputContainer.append(patientIDInput);
-
-        let reasonInputContainer = document.createElement("div");
-        let reasonIDInput = document.createElement("textarea");
-        reasonInputContainer.classList.add("container");
-        reasonIDInput.classList.add("inputBoxLarge");
-        reasonIDInput.id = "reasonInput";
-        reasonIDInput.placeholder = "Reason";
-        reasonInputContainer.append(reasonIDInput);
-
-        let buttonContainer = document.createElement("div");
-        buttonContainer.classList.add("container");
-
-        let submitButtonATag = document.createElement("a");
-        let submitButton = document.createElement("button");
-        submitButton.innerText = "Submit";
-        submitButtonATag.append(submitButton);
-        buttonContainer.append(submitButtonATag);
-
-        let blockingButton = document.createElement("button");
-        blockingButton.innerText = "Block Time";
-        buttonContainer.append(blockingButton);
-
-        bookable.append(patientInputContainer);
-        bookable.append(reasonInputContainer);
-        bookable.append(buttonContainer);
-
-        blockingButton.addEventListener("click", function() {
-            slot.classList.remove("timeBox");
-            slot.classList.add("unselectableTime");
+        if (slot.name == 'blocked') {
             bookable.innerHTML = "";
 
             let buttonContainer = document.createElement("div");
@@ -103,75 +128,156 @@ document.addEventListener("DOMContentLoaded", () => {
             freeingButton.innerText = "Unblock";
             buttonContainer.append(freeingButton);
             bookable.append(buttonContainer);
+        } else {
+            let patientInputContainer = document.createElement("div");
+            let patientIDLabelClassifier = document.createElement("div");
+            let patientIDLabel = document.createElement("span");
+            let patientIDInput = document.createElement("input");
+            patientInputContainer.classList.add("questionContainer");
+            patientIDLabelClassifier.classList.add("questionLabel");
+            patientIDInput.classList.add("inputBox");
+            patientIDInput.id = "PatientIDInput";
+            patientIDInput.placeholder = "Patient ID";
+            patientIDLabel.innerHTML = "Patient ID:";
+            patientIDLabelClassifier.append(patientIDLabel);
+            patientInputContainer.append(patientIDLabelClassifier);
+            patientInputContainer.append(patientIDInput);
 
-            let theSlot = slot.innerText + ":00";
-            let dateSelected = datePicker.value;
-            let formattedDateTime = dateSelected + " " + theSlot;
+            let reasonInputContainer = document.createElement("div");
+            let reasonIDInput = document.createElement("textarea");
+            reasonInputContainer.classList.add("container");
+            reasonIDInput.classList.add("inputBoxLarge");
+            reasonIDInput.id = "reasonInput";
+            reasonIDInput.placeholder = "Reason";
+            reasonInputContainer.append(reasonIDInput);
 
-            let xhr = new XMLHttpRequest();
+            let buttonContainer = document.createElement("div");
+            buttonContainer.classList.add("container");
 
-            let body = {
-                vetID: 1111,
-                bookingDate: formattedDateTime
-            };
+            if (slot.classList.contains('unselectableTime') && slot.name != 'bookable') {
+                for (const key in bookedSlots) {
+                    if (bookedSlots.hasOwnProperty(key)) {
+                        let bookingDate = key;
 
-            xhr.addEventListener("readystatechange", function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    let responseJSON = xhr.responseText;
-                    try {
-                        let response = JSON.parse(responseJSON);
-                    } catch (error) {
-                        console.log(error);
+                        let date = datePicker.value;
+                        let time = slot.id + ':00';
+                        let dateTime = date + ' ' + time
+                        if (dateTime == bookingDate) {
+                            let patientID = bookedSlots[key].patientID;
+                            let reason = bookedSlots[key].reason;
+
+                            patientIDInput.placeholder = patientID;
+                            reasonIDInput.placeholder = reason;
+
+                            let updateButton = document.createElement("button");
+                            updateButton.innerText = "Update";
+                            buttonContainer.append(updateButton);
+
+                            let deleteButton = document.createElement("button");
+                            deleteButton.innerText = "Delete";
+                            buttonContainer.append(deleteButton);
+                        }
                     }
                 }
-            });
+            } else {
+                let submitButton = document.createElement("button");
+                submitButton.innerText = "Submit";
+                buttonContainer.append(submitButton);
 
-            xhr.open("POST", "./api/booking/block.php?", true);
-            xhr.send(JSON.stringify(body));
+                let blockingButton = document.createElement("button");
+                blockingButton.innerText = "Block Time";
+                buttonContainer.append(blockingButton);
 
-            freeingButton.addEventListener("click", function() {
-                slot.classList.remove("unselectableTime");
-                slot.classList.add("timeBox");
-                bookable.innerHTML = "";
+                blockingButton.addEventListener("click", function() {
+                    slot.classList.remove("timeBox");
+                    slot.classList.add("unselectableTime");
+                    slot.name = "blocked";
+                    bookable.innerHTML = "";
 
-                fillBookableForm(bookable, slot);
-            });
-        });
+                    let buttonContainer = document.createElement("div");
+                    buttonContainer.classList.add("container");
 
-        
+                    let freeingButton = document.createElement("button");
+                    freeingButton.innerText = "Unblock";
+                    buttonContainer.append(freeingButton);
+                    bookable.append(buttonContainer);
 
-        submitButton.addEventListener("click", function() {
-            let theSlot = slot.innerText + ":00";
-            let dateSelected = datePicker.value;
-            let formattedDateTime = dateSelected + " " + theSlot;
+                    let theSlot = slot.innerText + ":00";
+                    let dateSelected = datePicker.value;
+                    let formattedDateTime = dateSelected + " " + theSlot;
 
-            slot.classList.remove("timeBox");
-            slot.classList.add("unselectableTime");
+                    let xhr = new XMLHttpRequest();
 
-            let xhr = new XMLHttpRequest();
+                    let body = {
+                        vetID: 1111,
+                        bookingDate: formattedDateTime
+                    };
 
-            let body = {
-                vetID: 1111,
-                patientID: patientIDInput.value,
-                bookingDate: formattedDateTime,
-                reason: reasonIDInput.value
-            };
+                    xhr.addEventListener("readystatechange", function() {
+                        if (xhr.readyState === XMLHttpRequest.DONE) {
+                            let responseJSON = xhr.responseText;
+                            try {
+                                let response = JSON.parse(responseJSON);
+                            } catch (error) {
+                                console.log(error);
+                            }
+                        }
+                    });
 
-            xhr.addEventListener("readystatechange", function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    let responseJSON = xhr.responseText;
-                    try {
-                        let response = JSON.parse(responseJSON);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-            });
+                    xhr.open("POST", "./api/booking/block.php?", true);
+                    xhr.send(JSON.stringify(body));
 
-            xhr.open("POST", "./api/booking/create.php?", true);
-            xhr.send(JSON.stringify(body));
+                    freeingButton.addEventListener("click", function() {
+                        freeingButton(slot);
+                        bookable.innerHTML = "";
+                        fillBookableForm(bookable, slot);
+                    });
+                });
+                submitButton.addEventListener("click", function() {
+                    let theSlot = slot.innerText + ":00";
+                    let dateSelected = datePicker.value;
+                    let formattedDateTime = dateSelected + " " + theSlot;
 
-            location.reload();
-        });
+                    slot.classList.remove("timeBox");
+                    slot.classList.add("unselectableTime");
+
+                    let xhr = new XMLHttpRequest();
+
+                    let body = {
+                        vetID: 1111,
+                        patientID: patientIDInput.value,
+                        bookingDate: formattedDateTime,
+                        reason: reasonIDInput.value
+                    };
+
+                    xhr.addEventListener("readystatechange", function() {
+                        if (xhr.readyState === XMLHttpRequest.DONE) {
+                            let responseJSON = xhr.responseText;
+                            try {
+                                let response = JSON.parse(responseJSON);
+                            } catch (error) {
+                                console.log(error);
+                            }
+                        }
+                    });
+
+                    xhr.open("POST", "./api/booking/create.php?", true);
+                    xhr.send(JSON.stringify(body));
+
+                    location.reload();
+                });
+            }
+
+            bookable.append(patientInputContainer);
+            bookable.append(reasonInputContainer);
+            bookable.append(buttonContainer);
+
+            
+        }
+
+        let freeingButton = function(slot) {
+            slot.classList.remove("unselectableTime");
+            slot.classList.add("timeBox");
+        }
     }
 });
