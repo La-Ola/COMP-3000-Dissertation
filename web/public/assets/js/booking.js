45bb1183991c667aefe5d33b658745403e34991a
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-02-22 20:11:46 +0000

    settings
    user can now set font size and theme

--- web/public/assets/js/booking.js
+++ web/public/assets/js/booking.js
@@ -447,6 +447,17 @@ document.addEventListener('DOMContentLoaded', () => {
         checkAvailability();
     });
 
+    if (localStorage.getItem('font') === 'small') {
+		document.body.setAttribute('font-size', 'small');
+        localStorage.setItem('font', 'small');
+    } else if (localStorage.getItem('font') === 'large') { 
+		document.body.setAttribute('font-size', 'large');
+        localStorage.setItem('font', 'large');
+    } else {
+        document.body.removeAttribute('font-size');
+        localStorage.removeItem('font');
+    }
+
     if (localStorage.getItem("theme") === "dark") { 
 		document.body.setAttribute("data-theme", "dark"); 
 	} else { 
