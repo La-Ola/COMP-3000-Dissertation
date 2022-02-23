ad4f19a5c2e3ba1d9426c70f7b489b81864e29ad
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-02-23 17:37:07 +0000

    diagnosis page added

---
+++ web/public/assets/js/diagnosis.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+
+    if (localStorage.getItem('font') === 'small') {
+		document.body.setAttribute('font-size', 'small');
+        localStorage.setItem('font', 'small');
+    } else if (localStorage.getItem('font') === 'large') { 
+		document.body.setAttribute('font-size', 'large');
+        localStorage.setItem('font', 'large');
+    } else {
+        document.body.removeAttribute('font-size');
+        localStorage.removeItem('font');
+    }
+
+    if (localStorage.getItem("theme") === "dark") { 
+		document.body.setAttribute("data-theme", "dark"); 
+	} else { 
+		document.body.removeAttribute("data-theme", "dark");
+	}
+});
--- web/public/diagnosis.php
+++ web/public/diagnosis.php
@@ -2,15 +2,15 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="./assets/css/settings/style.css">
+    <link rel="stylesheet" href="./assets/css/diagnosis/style.css">
     <link rel="stylesheet" href="./assets/css/global.css">
     <link rel="stylesheet" href="./assets/css/themes.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="./assets/awesome-notifications-master/dist/style.css" rel="stylesheet">
 
-    <script src="./assets/js/settings.js"></script>
+    <script src="./assets/js/diagnosis.js"></script>
     <script src="./assets/awesome-notifications-master/dist/index.var.js"></script>
-    <title>Settings</title>
+    <title>Calander</title>
 </head>
 
 <body>
@@ -25,18 +25,18 @@
                 <div class="navText">Github</div>
             </button>
         </a>
-        <a>
-            <button class="navbarActive">
-                <svg  class="svgFill" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <a href="./setting.php">
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 					<!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com/ License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
 					<path d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z" />
 				</svg>
                 <div class="navText">Settings</div>
             </button>
         </a>
-        <a href="./diagnosis.php">
-            <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <a>
+            <button class="navbarActive">
+            <svg class="svgFill" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                 <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
                 <path d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM96 424c-13.3 0-24-10.7-24-24s10.7-24 24-24 24 10.7 24 24-10.7 24-24 24zm0-96c-13.3 0-24-10.7-24-24s10.7-24 24-24 24 10.7 24 24-10.7 24-24 24zm0-96c-13.3 0-24-10.7-24-24s10.7-24 24-24 24 10.7 24 24-10.7 24-24 24zm96-192c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm128 368c0 4.4-3.6 8-8 8H168c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-96c0 4.4-3.6 8-8 8H168c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-96c0 4.4-3.6 8-8 8H168c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16z"/>
             </svg>
@@ -45,7 +45,7 @@
         </a>
         <a href="./booking.php">
             <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
                 <path d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"/>
             </svg>
@@ -65,37 +65,6 @@
     </div>
 
     <div class="newBody">
-        <div class="header">
-            Background Mode
-        </div>
-        <div class="card">Light Mode/Dark Mode
-            <div  id="toggle" class="toggle-container">
-                <div id="toggle-button" class="toggle-button"></div>
-            </div>
-        </div>
-
-        <div class="header">
-            Font Sizing
-        </div>
-        <div id="fontSize" class="card">Small/Medium/Large
-            <div class="fontContainer">
-                <button id="largeFont" class="fontBox">
-                    <svg class="fontLrgSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
-                    <path d="M432 416h-23.41L277.88 53.69A32 32 0 0 0 247.58 32h-47.16a32 32 0 0 0-30.3 21.69L39.41 416H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16h-19.58l23.3-64h152.56l23.3 64H304a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM176.85 272L224 142.51 271.15 272z"/></svg>
-                </button>
-                <button id="regularFont" class="fontBox fontBoxActive">
-                    <svg class="fontRegSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
-                    <path d="M432 416h-23.41L277.88 53.69A32 32 0 0 0 247.58 32h-47.16a32 32 0 0 0-30.3 21.69L39.41 416H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16h-19.58l23.3-64h152.56l23.3 64H304a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM176.85 272L224 142.51 271.15 272z"/></svg>
-                </button>
-                <button id="smallFont" class="fontBox">
-                    <svg class="fontSmlSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
-                    <path d="M432 416h-23.41L277.88 53.69A32 32 0 0 0 247.58 32h-47.16a32 32 0 0 0-30.3 21.69L39.41 416H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16h-19.58l23.3-64h152.56l23.3 64H304a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM176.85 272L224 142.51 271.15 272z"/></svg>
-                </button>
-            </div>
-        </div>
     </div>
 </body>
 </html>
