8863c3506669cbc691dfda135faa91481afa253b
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-04-07 19:36:22 +0100

    themes
    themes consistant across pages

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -10,5 +10,22 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err))
     })
 }*/
+document.addEventListener('DOMContentLoaded', () => {
 
+    if (localStorage.getItem('font') === 'small') {
+        document.body.setAttribute('font-size', 'small');
+        localStorage.setItem('font', 'small');
+    } else if (localStorage.getItem('font') === 'large') { 
+        document.body.setAttribute('font-size', 'large');
+        localStorage.setItem('font', 'large');
+    } else {
+        document.body.removeAttribute('font-size');
+        localStorage.removeItem('font');
+    }
 
+    if (localStorage.getItem("theme") === "dark") { 
+        document.body.setAttribute("data-theme", "dark"); 
+    } else { 
+        document.body.removeAttribute("data-theme", "dark");
+    }
+});
