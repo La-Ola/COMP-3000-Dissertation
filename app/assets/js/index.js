70ecf43cae6c4214fd80cd3365f9f16cee129abf
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2022-04-24 20:44:43 +0100

    service workers and settings
    service worker.js is done but not checked.
    setting.js is quality checked and has comments

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -2,14 +2,15 @@
  * firstly check to see if the browser supports the service worker.
  * on page load, it registers the service worker
  */
-/** 
-if ("serviceWorker" in navigator) {
+
+/**if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker.register("./serviceWorker.js", {updateViaCache: 'none'})
         .then(navigator.serviceWorker.update())
         .catch(err => console.log("service worker not registered", err))
     })
 }*/
+
 document.addEventListener('DOMContentLoaded', () => {
 
     if (localStorage.getItem('font') === 'small') {
--- app/serviceWorker.js
+++ app/serviceWorker.js
@@ -1,37 +1,90 @@
-//declaring a name for our cache
-const FamiliarSystems = "familiar-systems-app"
+/**
+ * caches.open() creates new cache called cache1
+ * cache1 is the first cacheof all the sites resources
+ * addAll() waits for cache to be created then takes an array of URLs to put into cache
+*/
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open('cache1');
+    await cache.addAll(resources);
+}
 
-//all things needed to run app, these need to be stored in the cache
-const asset = [
-    "./",
-    "./index.php",
-    "./assets/css/index/style.css",
-    "./assets/js/index.js",
-    "./templogo.png",
-]
-
-/**self refers to the service worker
- *this event is triggered as soon as a service worker is installed, only runs once per service worker
- *open() then creates cache, returns a promise and stores assets
- *this helps handle fetching data when offline
+/**
+ * clone gets added to the cache
  */
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(FamiliarSystems).then(cache => {
-            cache.addAll(asset);
-        })
-    )
-})
+const putInCache = async (request, response) => {
+    const cache = await caches.open('cache1');
+    await cache.put(request, response);
+}
 
 /**
  * to fetch assets
  * respondWith prevents browsers default response
  * once cache is ready, apply caches.match(...) to check if something in the cache matches the request.
+ * caches.match acts like a http request but to the cache
+ * 
+ * if request doesnt match, it runs a function to add page to cache for future
+ * promise not returned by put in cache because i dont want to wait for it to be added before returning a response
+ * 
+ * preload starts download immediatly so that it doesnt have to wait for service worker to boot up
  */
-self.addEventListener("fetch", fetchEvent => {
-    //fetchEvent.respondWith(
-        //caches.match(fetchEvent.request).then(res => {
-            //return res || fetch(fetchEvent.request)
-    //    })
-    //)
-})
+const cacheFirst = async (request, preloadResponsePromise) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+        console.info('using preload response', preloadResponse);
+        putInCache(request, preloadResponse.clone());
+        return preloadResponse;
+    }
+}
+
+// Enable navigation preloadin browser
+const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+        await self.registration.navigationPreload.enable();
+    }
+}
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(enableNavigationPreload());
+});
+
+/**
+ * self refers to the service worker
+ * service worker not installed until the 'waitUntil()' has successfully occured
+ * this helps handle fetching data when offline
+*/
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            '/app',
+            '/app/index.php',
+            '/app/assets/js/index.js',
+            '/app/appointment.php',
+            '/app/assets/css/appointment/style.css',
+            '/app/assets/js/appointment.js',
+            '/app/diagnosis.php',
+            '/app/assets/js/diagnosis.js',
+            '/app/profile.php',
+            '/app/assets/css/profile/style.css',
+            '/app/assets/js/profile.js',
+            '/app/setting.php',
+            '/app/assets/css/setting/style.css',
+            '/app/assets/js/setting.js',
+            '/app/assets/css/global.css',
+            '/app/assets/css/themes.css',
+            '/app/familiarsystems.ico',
+        ])
+    )
+});
+
+self.addEventListener('fetch', (fetchEvent) => {
+    fetchEvent.respondWith(
+        cachesFirst({
+            request: fetchEvent.request,
+            preloadResponsePromise: fetchEvent.preloadResponse
+        })
+    )
+});
